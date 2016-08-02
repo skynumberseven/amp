119f53b95ad8b20a138944833633a23508627c62
skynumberseven <1227789982@qq.com>
2016-08-02 11:30:20 +0800

    update

---
+++ xjj.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>main</title>
+</head>
+<body>
+<table width="100%" border="0">
+  <tr>
+    <td colspan="2" style="background-color:#FFA500;">
+      <h1>账号管理平台.txy</h1>
+    </td>
+  </tr>
+  <tr>
+    <td style="background-color:#FFD700;width:80px;">
+      <b><h3>资源管理</h3></b>
+      <a href=" ">< img src="10.jpg" alt="" width="80" height="80"/>修改</a >
+        <a href="#">< img src="8.jpg" alt="" width="80" height="80"/>添加</a >
+          <a href="#">< img src="9.png" alt="" width="80" height="80"/>删除</a >
+          </td>
+        <td style="background-color:#eeeeee;height:500px;width:400px;">
+            <a href="https://wx.qq.com/">< img src="1.jpg" alt="" width="200" height="200"/>微信</a >
+              <a href="https://www.taobao.com/">< img src="2.jpg" alt="" width="280" height="200"/>淘宝</a >
+                <a href="http://mail.163.com/">< img src="3.jpg" alt="" width="200" height="200"/>163</a >
+                  <a href="http://www.4399.com/">< img src="4.jpg" alt="" width="200" height="200"/>小游戏</a >
+                    <a href="http://www.iqiyi.com/">< img src="5.jpg" alt="" width="200" height="200"/>爱奇艺</a >
+                      <a href="http://www.jd.com/">< img src="6.jpg" alt="" width="200" height="200"/>京东</a >
+                        <a href="http://www.kuwo.cn/">< img src="7.jpg" alt="" width="200" height="200"/>酷我</a >
+
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colspan="2" style="background-color:#FFA500;text-align:center;">
+                          金天带唐勋,叶祥康,徐佳佳作品</td>
+                        </tr>
+                      </table>
+                      if(isset($_GET["http://mail.163.com/http://mail.163.com/"]))
+                      $id = $_GET["http://mail.163.com/"];
+                    </body>
+                    </html>」
